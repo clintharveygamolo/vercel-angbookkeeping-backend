c017113afef930fa55ab1dd0dac43ed84e870b34
clintafk <csheesh34@protonmail.com>
2024-04-19 07:56:56 +0800

    Fixed docker config errors

--- index.js
+++ index.js
@@ -1,50 +1,66 @@
 import express from "express";
-import mariadb from 'mariadb';
 import mysql from "mysql";
 
 const app = express();
 const port = 9000;
 
-const pool = mariadb.createPool({
-    host: 'db',
-    user: 'admin',
-    password: 'secretadmin',
-    connectionLimit: 5
+// const pool = mariadb.createPool({
+//     host: 'db',
+//     user: 'admin',
+//     password: 'secret'
+// });
+// pool.getConnection()
+//     .then(conn => {
+//         conn.query("SELECT 1 as val") 
+//             .then((rows) => {
+//                 console.log(rows);
+//                 return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+//             })
+//             .then((res) =>{
+//                 console.log(res);
+//                 conn.end
+//             })
+//             .catch(err => {
+//                 console.log(err);
+//                 conn.end();
+//             })
+//         }).catch(err => {
+//         });
+
+app.listen(port, () => {
+    console.log(`${port} is running`);
 });
 
-// const connection = mysql.createConnection({
-//     host: 'db',
-//     user: 'test',
-//     password: 'secret',
-// });
+const connection = mysql.createConnection({
+    host: 'mariadb',
+    user: 'admin',
+    password: 'secret',
+});
 
-// connection.connect()
-// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-//     if(err) throw err
+connection.connect()
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if(err) throw err
 
-//     console.log("The solution is: ", rows[0].solution)
-// });
-// connection.end()
+    console.log("The solution is: ", rows[0].solution)
+});
+connection.end()
 
 app.get("/", (req, res) => {
     res.send("Hello, world");
 });
 
-app.get('/users', async (req, res) => {
-    let conn;
+// app.get('/users', async (req, res) => {
+//     let conn;
 
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query('SELECT * FROM users');
-        res.json(rows);
-    } catch(err) {
-        console.error(err);
-        res.status(500).send('Error retrieving users');
-    } finally {
-        if (conn) conn.release();
-    }
-});
+//     try {
+//         conn = await pool.getConnection();
+//         const rows = await conn.query('SELECT 1 + 1 AS solution');
+//         res.json(rows);
+//     } catch(err) {
+//         console.error(err);
+//         res.status(500).send('Error retrieving users');
+//     } finally {
+//         if (conn) conn.release();
+//     }
+// });
 
-app.listen(port, () => {
-    console.log(`${port} is running`);
-});
