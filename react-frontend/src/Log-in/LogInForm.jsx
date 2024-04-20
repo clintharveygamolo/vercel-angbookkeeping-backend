5c979ae36740d744de6ff755691498a75d3f4c8b
DinShaneDominicMagallanes <200930191@my.xu.edu.ph>
2024-04-20 23:47:30 +0800

    Completed Login Page Setup

--- react-frontend/src/Log-in/LogInForm.jsx
+++ react-frontend/src/Log-in/LogInForm.jsx
@@ -2,25 +2,28 @@ import React from 'react'
 import './LogInForm.css'
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
-
+import Anglogo from '/src/image/AngBookkeeping.png'
 
 const LogInForm = () => {
     return (
-        <div className='wrapper'>
-            <form action="">
-                <h1>
-                    Login
-                </h1>
-                <div className="input-box">
-                    <input type="text" placeholder='Username' required />
-                    <FaUser className='icon' />
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder='Password' required />
-                    <FaLock className='icon' />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+        <div classname='outsidewrapper'>
+            <img src={Anglogo} alt="Logo" className="AngBookkeepingLogo" />
+            <div className='wrapper'>
+                <form action="">
+                    <h1>
+                        Login
+                    </h1>
+                    <div className="input-box">
+                        <input type="text" placeholder='Username' required />
+                        <FaUser className='icon' />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder='Password' required />
+                        <FaLock className='icon' />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
     )
 }
