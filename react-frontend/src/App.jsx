332d6f640951a2f081d26d1af55f41cab9504a62
DinShaneDominicMagallanes <200930191@my.xu.edu.ph>
2024-04-21 20:51:04 +0800

    Added Sidebar Component

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import LogInForm from './Log-in/LogInForm'
+import Sidebar from './Components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    //<div>
+    //<LogInForm />
+    //</div>
     <div>
-      <LogInForm />
+      <Sidebar />
     </div>
   )
 }
