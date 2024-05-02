fe0320094217ea179f7a8a5b04069ca168a0a23a
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-03 01:19:48 +0800

    Added editing non-existing error

--- backend/controllers/editDepositsController.js
+++ backend/controllers/editDepositsController.js
@@ -5,8 +5,16 @@ export async function editDeposits(req, res) {
     try {
         const currentUser = await User.findByPk(req.body.user_id);
 
+        const { deposit_id } = req.body;
+
+        const deposits = await Deposits.findOne({ where: {deposit_id: deposit_id} });
+
         if(currentUser.role !== 'Admin') {
             return res.status(403).json({ error: "Forbidden: Only admin users can edit deposit entries." });
+        }
+
+        if(!deposits) {
+            return res.status(401).json({ message: "Updated failed, withdrawal entry not found."});
         }
 
         const editDeposits = await Deposits.update({
