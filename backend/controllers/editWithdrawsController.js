a56e46e2f770969c02b19d7d6078bdfc63c45e56
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-02 21:12:41 +0800

    Implemented the response of editing non-existing entries.

--- backend/controllers/editWithdrawsController.js
+++ backend/controllers/editWithdrawsController.js
@@ -4,9 +4,17 @@ import User from '../models/userModel.js';
 export async function editWithdraws(req, res) {
     try {
         const currentUser = await User.findByPk(req.body.user_id);
+        
+        const { withdraw_id } = req.body;
+
+        const withdraws = await Withdraws.findOne({ where: {withdraw_id: withdraw_id} });
 
         if(currentUser.role !== 'Admin') {
             return res.status(403).json({ error: "Forbidden: Only admin users can edit withdrawal entries." });
+        }
+
+        if(!withdraws) {
+            return res.status(401).json({ message: "Updated failed, withdrawal entry not found."})
         }
 
         const editWithdraws = await Withdraws.update({
