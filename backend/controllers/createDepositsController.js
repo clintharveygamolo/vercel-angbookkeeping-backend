746e4546cb99557efe4dd65ff740d57807803c8b
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-03 23:00:18 +0800

    Edited successfull deposit entry creation response.

--- backend/controllers/createDepositsController.js
+++ backend/controllers/createDepositsController.js
@@ -2,7 +2,7 @@ import Deposits from '../models/depositsModel.js';
 
 export async function createDeposits(req, res) {
     try {
-        const newDeposits = await Deposits.create({
+        await Deposits.create({
             deposit_id: req.body.deposit_id,
             particular: req.body.particular,
             date: req.body.date,
@@ -10,7 +10,7 @@ export async function createDeposits(req, res) {
             remarks: req.body.remarks
         });
 
-        res.status(201).json(newDeposits);
+        res.status(201).json("Creation success, deposit entry has been created.");
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "An error occured while creating the entry."});
