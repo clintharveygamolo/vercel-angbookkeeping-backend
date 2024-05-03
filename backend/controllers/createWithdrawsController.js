7a3ffdf0c6914c34a30122941729316c749db4b4
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-03 22:43:52 +0800

    Edited successful entry edit response.

--- backend/controllers/createWithdrawsController.js
+++ backend/controllers/createWithdrawsController.js
@@ -2,7 +2,7 @@ import Withdraws from '../models/withdrawsModel.js';
 
 export async function createWithdraws(req, res) {
     try {
-        const newWithdraws = await Withdraws.create({
+        await Withdraws.create({
             withdraw_id: req.body.withdraw_id,
             date: req.body.date,
             payee: req.body.payee,
@@ -12,7 +12,7 @@ export async function createWithdraws(req, res) {
             remarks: req.body.remarks
         });
 
-        res.status(201).json(newWithdraws);
+        res.status(201).json("Creation success, withdrawal entry has been created.");
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "An error occured while creating the entry."});
