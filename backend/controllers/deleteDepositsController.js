fc094074cba2a23b09f26a7ef4f1ab1715d29700
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-03 23:01:01 +0800

    Edited successful deposit entry deletion response.

--- backend/controllers/deleteDepositsController.js
+++ backend/controllers/deleteDepositsController.js
@@ -1,8 +1,13 @@
 import Deposits from '../models/depositsModel.js';
+import User from '../models/userModel.js';
 
-export async function deleteDesposits(req, res) {
+export async function deleteDeposits(req, res) {
     try {
         const currentUser = await User.findByPk(req.body.user_id);
+
+        const { deposit_id } = req.body;
+
+        const deposits = await Deposits.findOne({ where: {deposit_id: deposit_id} });
 
         if (currentUser.role !== 'Admin') {
             return res.status(403).json({ error: "Forbidden: Only admin users can delete deposit entries." });
@@ -19,7 +24,7 @@ export async function deleteDesposits(req, res) {
         }
         );
 
-        res.status(201).json();
+        res.status(201).json("Deletion successful, deposit entry has been deleted.");
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "An error occured while deleting the entry." });
