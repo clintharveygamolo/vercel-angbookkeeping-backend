4ecd20759a80362874e2e8eb6d73b74367bd8f3f
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-11 21:16:49 +0800

    Deleted files that were no longer needed due to compilation.

--- backend/routes/createWithdrawsRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createWithdraws } from '../controllers/createWithdrawsController.js';
-
-const router = express.Router();
-
-router.post('/createWithdraw', createWithdraws);
-
-export default router;
--- backend/routes/deleteWithdrawsRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import isAuth from '../middleware/isAuth.js';
-import { deleteWithdraws } from '../controllers/deleteWithdrawsController.js'
-const router = express.Router();
-
-router.post('/deleteWithdraw', isAuth, deleteWithdraws);
-
-export default router;
