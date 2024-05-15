82648cb99690dbbe24555c51b6abf7bb8efbe403
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-15 20:32:39 +0800

    Optimized Withdrawal Creation Entry.

--- backend/models/withdrawsModel.js
+++ backend/models/withdrawsModel.js
@@ -1,35 +1,49 @@
 import sequelize from '../util/database.js';
 import { DataTypes } from 'sequelize';
+import { format } from 'date-fns';
 
 const Withdraw = sequelize.define("Withdraw", {
     withdraw_id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        allowNull: false
+        allowNull: false,
+        autoIncrement: true
     },
     date: {
-        type: DataTypes.DATE,
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        get() {
+            const rawValue = this.getDataValue('date');
+            return format(new Date(rawValue), 'MM/dd/yyyy');
+        }
+    },
+    check_no: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    voucher_no: {
+        type: DataTypes.INTEGER,
         allowNull: false
     },
     payee: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    check_no: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    invoice_no: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    amount: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
     remarks: {
         type: DataTypes.STRING,
         allowNull: true
+    },
+    amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+            isDecimal: true, // Ensures that the value is a valid decimal number
+            isNotNegative(value) {
+                if (parseFloat(value) <= 0) {
+                    throw new Error('Amount must be a positive number');
+                }
+            }
+        }
     }
 });
 
