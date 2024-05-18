5f58541bbc72a0355999ea1b2bd0129a862a6564
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-18 16:05:35 +0800

    Removed relations.

--- backend/models/CompanyModel.js
+++ backend/models/CompanyModel.js
@@ -1,8 +1,5 @@
 import sequelize from '../util/database.js';
 import { DataTypes } from 'sequelize';
-import Bank from './BankModel.js';
-import BankCode from "./bankCodeModel";
-import Account from './accountModel.js';
 
 const Company = sequelize.define("Company", {
     company_id: {
@@ -16,9 +13,5 @@ const Company = sequelize.define("Company", {
         allowNull: false
     }
 });
-
-Company.hasMany(Account, { foreignKey: 'company_id' });
-Company.hasMany(Bank, { foreignKey: 'company_id' });
-Company.hasMany(BankCode, { foreignKey: 'company_id' });
 
 export default Company;
