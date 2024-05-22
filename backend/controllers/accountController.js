e2502ea8d7098ee827531808854be2e04c6d0449
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-22 20:47:39 +0800

    Added the account edit and delete function.

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -30,11 +30,90 @@ export const createAccount = async (req, res) => {
         const account = await Account.create({
             bankCode: bankCode,
             account_number: account_number,
-            company_id: company.id, // Use the id of the existing company
-            bank_id: bank.id,       // Use the id of the existing bank
+            company_id: company.id,
+            bank_id: bank.id,
         });
 
-        res.status(201).json({ message: "Account created successfully", company, bank, account });
+        res.status(201).json({ message: "Account Created Successfully", company, bank, account });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const editAccount = async (req, res) => {
+    try {
+        const { user_id, account_number, company_id, bank_id, bankCode, newBankCode } = req.body;
+
+        // Find the current user
+        const currentUser = await User.findByPk(user_id);
+
+        if (!currentUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        if (currentUser.role !== 'Admin') {
+            return res.status(403).json({ error: "Forbidden: Only admin users can edit accounts." });
+        }
+
+        const company = await Company.findByPk(company_id);
+        if (!company) {
+            return res.status(404).json({ error: "Company not found" });
+        }
+
+        const bank = await Bank.findByPk(bank_id);
+        if (!bank) {
+            return res.status(404).json({ error: "Bank not found" });
+        }
+
+        if (!bankCode) {
+            return res.status(400).json({ error: "Current bank code not provided." });
+        }
+
+        const account = await Account.findOne({ where: { bankCode: bankCode } });
+
+        if (!account) {
+            return res.status(404).json({ error: "Account not found" });
+        }
+
+        await account.update({
+            bankCode: newBankCode || bankCode,
+            account_number: account_number,
+            company_id: company.id,
+            bank_id: bank.id,
+        });
+
+        res.status(201).json({ message: "Account Edited Successfully.", company, bank, account });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+//Account Deletion Function
+export const deleteAccount = async (req, res) => {
+    try {
+        const { user_id, bankCode } = req.body;
+
+        const currentUser = await User.findByPk(user_id);
+
+        if (!currentUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        if (currentUser.role !== 'Admin') {
+            return res.status(403).json({ error: "Forbidden: Only admin users can create accounts." });
+        }
+
+        if (!bankCode) {
+            return res.status(401).json({ message: "Deletion failed, bank code not found." });
+        }
+
+        await Account.destroy({
+            where: {
+                bankCode: bankCode
+            }
+        }
+        );
+
+        res.status(201).json("Account Deleted Successfully.");
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
