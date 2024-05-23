aae25de086037c7a32e1dd77f018c11fa171538a
StephenDaveAng <20210021634@my.xu.edu.ph>
2024-05-23 22:36:50 +0800

    Modified the delete user function to factor in the function that starts user ID with an 1000.

--- backend/controllers/deleteUserController.js
+++ backend/controllers/deleteUserController.js
@@ -1,15 +1,16 @@
 import User from '../models/userModel.js';
 
 export async function deleteUser(req, res) {
-    const [ user_id ] = req.params.user_id;
+    const user_id = req.params.user_id;
+
     try {
         const user = await User.findByPk(user_id);
         if (!user) {
-            res.status(404).json({ message: "User not found!"});
+            return res.status(404).json({ message: "User not found!" });
         }
         await user.destroy();
-        res.status(200).json({ message: "User successfully deleted!"});
+        return res.status(200).json({ message: "User successfully deleted!" });
     } catch (error) {
-        res.status(400).json({ message: "An error occured while attempting to delete users!" })
+        return res.status(400).json({ message: "An error occurred while attempting to delete users!"});
     }
 };
