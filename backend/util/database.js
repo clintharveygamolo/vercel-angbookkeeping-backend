efb8495abcbb66ddb1afa5a10667ce97996865af
clintharveygamolo <159156893+clintharveygamolo@users.noreply.github.com>
2024-05-29 01:06:00 +0800

    Update database.js

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 
+// Use the external URL for connection
 const sequelize = new Sequelize(
-  process.env.DATABASE_DB,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
+  'angbookkeeping_db', // Database name
+  'angbookkeeping_db_user', // Username
+  '3hNT8qPOfrOg6WuQVEpkjJw8Tj5Nn2NA', // Password
   {
-    host: process.env.DATABASE_HOST,
-    dialect: "mariadb",
+    host: 'dpg-cpamtjm3e1ms739s2eh0-a.singapore-postgres.render.com', // Hostname
+    port: 5432, // Port
+    dialect: 'postgres', // Dialect
     dialectOptions: {
       connectTimeout: 10000,
     },
